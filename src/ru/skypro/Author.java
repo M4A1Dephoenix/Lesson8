38e2bf24202e88059c17b5f58a9df1e334072690
M4A1Dephoenix <67238335+M4A1Dephoenix@users.noreply.github.com>
2021-11-10 21:52:50 +0300

    Easy level 9 lesson

--- src/ru/skypro/Author.java
+++ src/ru/skypro/Author.java
@@ -1,11 +1,33 @@
 package ru.skypro;
 
+import java.util.Objects;
+
 public class Author {
     final String name;
     final String surName;
     public Author(String name, String surName){
         this.name = name;
         this.surName = surName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surName, author.surName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surName);
     }
 
     public String getName(){
--- src/ru/skypro/Book.java
+++ src/ru/skypro/Book.java
@@ -1,5 +1,7 @@
 package ru.skypro;
 
+import java.util.Objects;
+
 public class Book {
     final String bookName;
     final Author authorName;
@@ -10,7 +12,33 @@ public class Book {
         this.bookName = bookName;
         this.authorName = authorName;
         yearPublication = yearPublication;
+
     }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", authorName=" + authorName.toString() +
+                ", yearPublication=" + yearPublication +
+                '}';
+    }
+
+    @Override
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearPublication == book.yearPublication && Objects.equals(bookName, book.bookName) && Objects.equals(authorName, book.authorName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, authorName, yearPublication);
+    }
+
     public String getbookName(){
         return this.bookName;
     }
